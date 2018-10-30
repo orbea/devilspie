83921e47dadc09682badd9a7abe805c61075a417
orbea <orbea@fredslev.dk>
2018-10-30 09:31:01 -0700

    Don't depend on gnome-common.

--- autogen.sh
+++ autogen.sh
@@ -6,15 +6,10 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="devilspie"
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-which gnome-autogen.sh || {
-	echo "You need to install gnome-common from the GNOME CVS"
-	exit 1
-}
-
-REQUIRED_AUTOMAKE_VERSION=1.6 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+autoreconf -i
